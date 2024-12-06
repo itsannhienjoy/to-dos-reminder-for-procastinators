22cc064d72373e463d61820173eefe8869844def
itsannhienjoy <dophuongannhien3105@gmail.com>
2024-12-07 00:08:33 +1100

    added edit function

---
+++ src/components/EditTodoForm.js
@@ -0,0 +1,16 @@
+import React, {useState} from 'react'
+
+export const EditTodoForm = ({editTodo, task}) => {
+    const [value, setValue] = useState(task.task);
+
+    const handleSubmit = (e) => {
+        e.preventDefault(); // prevent default action
+        editTodo(value, task.id);
+      };
+  return (
+    <form onSubmit={handleSubmit} className="todoForm">
+    <input type="text" value={value} onChange={(e) => setValue(e.target.value)} className="todo-input" placeholder='Update task' />
+    <button type="submit" className='todo-btn'>Add Task</button>
+  </form>
+  )
+}

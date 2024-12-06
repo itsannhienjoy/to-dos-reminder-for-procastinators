4ba74d94bdf1ddfb7fe16c01484edee2b95fca4a
itsannhienjoy <dophuongannhien3105@gmail.com>
2024-12-06 15:00:04 +1100

    added components

---
+++ src/components/TodoWrapper.js
@@ -0,0 +1,23 @@
+import React, { useState } from 'react'
+import { TodoForm } from './TodoForm'
+import { Todo } from './Todo'
+import { v4 as uuidv4 } from 'uuid'
+uuidv4();
+
+export const TodoWrapper = () => {
+    const [todos, setTodos] = useState([])
+
+    const addTodo = todo => {
+        setTodos([...todos, {id: uuidv4(), task: todo, completed: false, isEditing: false}])
+        console.log(todos) 
+    }
+    return (
+        <div className='todoWrapper'>
+            <h1>Get Things Done!</h1>
+            <TodoForm addTodo={addTodo}/> 
+            {todos.map((todo, index) => {
+                <Todo task={todo}  key={index} />
+            })}
+        </div>
+    )
+}

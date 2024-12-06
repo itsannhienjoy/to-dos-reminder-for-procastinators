7260ca1c426443b58d5923bf563b1b0c0867f74c
itsannhienjoy <dophuongannhien3105@gmail.com>
2024-12-06 22:24:15 +1100

    fixed todo

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -3,10 +3,12 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faPenToSquare } from '@fortawesome/free-regular-svg-icons'
 import { faTrash } from '@fortawesome/free-solid-svg-icons'
 
-export const Todo = ({task}) => {
+export const Todo = ({task, toggleComplete}) => {
     return (
         <div className='todo'>
-            <p className={`${task.completed ? "complete" : "incomplete"}`}>{task.task}</p>
+            <p onClick={() => toggleComplete(task.id)} 
+            className={`${task.completed ? "completed" : "incompleted"}
+            `}>{task.task}</p>
             <div>
                 <FontAwesomeIcon icon={faPenToSquare} />
                 <FontAwesomeIcon icon={faTrash} />
